a860a05b8c311dd736231a72ef3e9723b7459aea
Gloryozo <gloryozoji@gmail.com>
2024-09-27 14:06:06 +0300

    Updated hello message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello World! This is Glory's render PAAS app");
+    res.send("Hello This is Glory from render");
 });
 
 app.listen(PORT, () => {
